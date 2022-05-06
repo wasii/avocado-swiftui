c78e98dd2c036bd43af241e4421fee62fd3e0a4e
abdWasiq <abdul.wasiq@naheed.pk>
2022-05-06 11:26:40 +0500

    project created

---
+++ Avacados/AvacadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvacadosApp.swift
+//  Avacados
+//
+//  Created by NaheedPK on 06/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct AvacadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
